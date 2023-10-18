e3164cf36e38687a7f3d3916f931331bd34aa5c7
Graham Miln <graham.miln@dssw.co.uk>
2023-10-18 11:44:37 +0200

    CalSub v1.2.2

--- Casks/miln-calsub.rb
+++ Casks/miln-calsub.rb
@@ -1,8 +1,8 @@
 cask "miln-calsub" do
-  version "1.2.1+20231016.102816"
-  sha256 "0427927d3890ba286c5276622e015f3cb406036998ef167365bd332ee6c02329"
+  version "1.2.2+20231018.113654"
+  sha256 "2cc1f607b4d7560be5fb5bb5520498f546ad0863c805ebef3fc3c58abce4c7fa"
 
-  url "https://miln.eu/calsub/miln-calsub-v1.2.1-darwin-universal.zip"
+  url "https://miln.eu/calsub/miln-calsub-v1.2.2-darwin-universal.zip"
   name "CalSub"
   desc "Import iCalendar (ics) events into a CalDAV calendar"
   homepage "https://miln.eu/calsub"
@@ -13,5 +13,5 @@ cask "miln-calsub" do
     regex(/miln-calsub[._-]v?(\d+(?:\.\d+)+)-darwin-universal\.zip/)
   end
 
-  binary "miln-calsub-v1.2.1-darwin-universal/calsub"
+  binary "miln-calsub-v1.2.2-darwin-universal/calsub"
 end
