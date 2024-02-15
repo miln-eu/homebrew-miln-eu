fa2a3b7b0ebe185fab6868930dfde91845a84434
Graham Miln <graham.miln@dssw.co.uk>
2024-02-15 20:34:26 +0100

    Keysafe v1.5.0

--- Casks/miln-keysafe.rb
+++ Casks/miln-keysafe.rb
@@ -1,8 +1,8 @@
 cask "miln-keysafe" do
-  version "1.4.0+20240214.125956"
-  sha256 "b1a83bba3031745228ed4e20e795340eac738b440cfe0792c5d7b1f5889d525e"
+  version "1.5.0+20240215.202739"
+  sha256 "7a47630e6fc0b83de0640aeb355907bd89d29fb0185e1398d66e08dabc1ded40"
 
-  url "https://miln.eu/keysafe/miln-keysafe-v1.4.0-darwin-universal.zip"
+  url "https://miln.eu/keysafe/miln-keysafe-v1.5.0-darwin-universal.zip"
   name "Keysafe"
   desc "Read and decrypt Keychain files"
   homepage "https://miln.eu/keysafe"
@@ -13,5 +13,5 @@ cask "miln-keysafe" do
     regex(/miln-keysafe[._-]v?(\d+(?:\.\d+)+)-darwin-universal\.zip/)
   end
 
-  binary "miln-keysafe-v1.4.0-darwin-universal/keysafe"
+  binary "miln-keysafe-v1.5.0-darwin-universal/keysafe"
 end
