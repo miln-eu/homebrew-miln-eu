699d7dd499e5000623ae717d8d97861aac163c8a
Graham Miln <278765+grahammiln@users.noreply.github.com>
2025-11-06 14:14:09 +0100

    Check Expiry v1.2.0

--- Casks/miln-check-expiry.rb
+++ Casks/miln-check-expiry.rb
@@ -1,8 +1,8 @@
 cask "miln-check-expiry" do
-  version "1.1.0+20250902.113803"
-  sha256 "8819e78aece7fd0ea1a36e52b4416df3b1275e352f8a8e0dd2cae319385cfd61"
+  version "1.2.0+20251106.141122"
+  sha256 "bf6311090973872a814513033a556b02e57b2454fa70c211a6ea6263a74a60d7"
 
-  url "https://miln.eu/checkexpiry/miln-check-expiry-v1.1.0-darwin-universal.zip"
+  url "https://miln.eu/checkexpiry/miln-check-expiry-v1.2.0-darwin-universal.zip"
   name "Check Expiry"
   desc "Check expiry dates of certificates and security tokens"
   homepage "https://miln.eu/checkexpiry"
@@ -13,5 +13,5 @@ cask "miln-check-expiry" do
     regex(/miln-check-expiry[._-]v?(\d+(?:\.\d+)+)-darwin-universal\.zip/)
   end
 
-  binary "miln-check-expiry-v1.1.0-darwin-universal/checkexpiry"
+  binary "miln-check-expiry-v1.2.0-darwin-universal/checkexpiry"
 end
